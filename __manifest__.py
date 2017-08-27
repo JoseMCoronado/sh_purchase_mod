37c1353915dca96f6b4ddd7c042456f67802a6b5
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-08-27 15:33:03 -0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Commissioned Purchase/Inventory Management Modifications',
+    'category': 'Purchase',
+    'author': 'GFP Solutions',
+    'summary': 'Custom',
+    'version': '1.0',
+    'description': """
+Purchase/Inventory Management modifications commissioned by Speedhut. Check Flow for modification details.
+
+THIS MODULE IS PROVIDED AS IS - INSTALLATION AT USERS' OWN RISK - AUTHOR OF MODULE DOES NOT CLAIM ANY
+RESPONSIBILITY FOR ANY BEHAVIOR ONCE INSTALLED.
+        """,
+
+    'depends':['base','account_accountant','purchase','stock'],
+    'data':['views/stock_picking_return_views.xml','views/purchase_views.xml'],
+    'installable': True,
+}
---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+import purchase
+import product
+import purchase_line_return
