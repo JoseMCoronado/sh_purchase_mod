7ac1e9155e615d6e99575c784dd3ba469522a209
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-09-02 14:26:16 -0700

    Moved MrpProduction mod to new module

--- models/purchase.py
+++ models/purchase.py
@@ -163,23 +163,9 @@ class AccountInvoice(models.Model):
             data['account_id'] = account.id
         return data
 
-class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
-
-    @api.multi
-    def _generate_moves(self):
-        for production in self:
-            production._generate_finished_moves()
-            factor = production.product_uom_id._compute_quantity(production.product_qty, production.bom_id.product_uom_id) / production.bom_id.product_qty
-            boms, lines = production.bom_id.explode(production.product_id, factor, picking_type=production.bom_id.picking_type_id)
-            production._generate_raw_moves(lines)
-            production._adjust_procure_method()
-            production.move_raw_ids.action_cancel()
-        return True
-
 class PackOperation(models.Model):
     _inherit = "stock.pack.operation"
-    
+
     @api.multi
     def _compute_location_description(self):
         for operation, operation_sudo in zip(self, self.sudo()):
